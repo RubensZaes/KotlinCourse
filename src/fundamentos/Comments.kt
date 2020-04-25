0862547cd5c66a2b7d25715b47abfda2b6ce36cd
RubensZaes <rubenszaes@gmail.com>
2020-04-25 11:36:29 -0300

    Comments

---
+++ src/fundamentos/Comments.kt
@@ -0,0 +1,19 @@
+package fundamentos
+
+/**
+ * Kotlin suporta KDoc...
+ * @param args lista de parâmetros passados por linha de comando
+ */
+
+fun main(args: Array<String>) {
+    // Comentário de uma linha
+    println("Opa")
+    /*
+    Mais de uma linha
+     */
+    println("xD")
+    /*
+    *Mais de
+    * uma linha
+     */
+}
